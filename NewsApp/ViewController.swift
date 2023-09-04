7a9d3b909813a3c1b1a03be76e32c4563bbb7bd5
Ecem Ozturk <ecemozturk704@gmail.com>
2023-09-04 23:20:59 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ecem Öztürk on 4.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
