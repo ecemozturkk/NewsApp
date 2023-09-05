c3e621c9a755248b0164c4a3a2159e5703ca0f62
Ecem Ozturk <ecemozturk704@gmail.com>
2023-09-05 23:58:48 +0300

    onboarding first step

--- NewsApp/Views/Onboarding/OnboardingViewController.swift
+++ NewsApp/Views/Onboarding/OnboardingViewController.swift
@@ -1,19 +1,20 @@
 //
-//  ViewController.swift
+//  OnboardingViewController.swift
 //  NewsApp
 //
-//  Created by Ecem Öztürk on 4.09.2023.
+//  Created by Ecem Öztürk on 5.09.2023.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
