8525902a29604df2579d82507a43df83e1882d88
Ecem Ozturk <ecemozturk704@gmail.com>
2023-09-06 00:34:26 +0300

    cornerRadius feature for button added

---
+++ NewsApp/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  NewsApp
+//
+//  Created by Ecem Öztürk on 6.09.2023.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title, description: String
+    let image: UIImage
+    
+}
--- NewsApp/Views/Onboarding/OnboardingViewController.swift
+++ NewsApp/Views/Onboarding/OnboardingViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    var slides: [OnboardingSlide] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func nextBtnClicked(_ sender: UIButton) {
+    }
+    
 
 }
