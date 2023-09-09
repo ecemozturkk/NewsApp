730778dfbc308d74135abc346b0a416ae035f65d
Ecem Ozturk <ecemozturk704@gmail.com>
2023-09-09 23:36:24 +0300

    sign in/sign up UI added

---
+++ NewsApp/Views/SignIn/SignInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignInViewController.swift
+//  NewsApp
+//
+//  Created by Ecem Öztürk on 9.09.2023.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+    }
+    
+}
