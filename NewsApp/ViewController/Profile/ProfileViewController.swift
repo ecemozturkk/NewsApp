44a5dbf508b735d706c90efd5cf426695083b3d3
Ecem Ozturk <ecemozturk704@gmail.com>
2023-09-21 00:59:45 +0300

    profile screen

--- NewsApp/ViewController/Profile/ProfileViewController.swift
+++ NewsApp/ViewController/Profile/ProfileViewController.swift
@@ -12,6 +12,7 @@ class ProfileViewController: UIViewController {
     
     
     @IBOutlet weak var switchTheme: UISwitch!
+    @IBOutlet weak var emailLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,15 @@ class ProfileViewController: UIViewController {
         if #available(iOS 13.0, *) {
             let darkMode = UserDefaults.standard.bool(forKey: "darkMode")
             switchTheme.isOn = darkMode
+        }
+        
+        if let user = Auth.auth().currentUser {
+            if let userEmail = user.email {
+                // Kullanıcının e-posta adresi userEmail değişkeninde bulunur.
+                // Şimdi bu e-posta adresini ProfileViewController'daki bir label'da gösterebilirsiniz.
+                // Örnek olarak, "emailLabel" adında bir label kullanalım:
+                emailLabel.text = userEmail
+            }
         }
     }
     
